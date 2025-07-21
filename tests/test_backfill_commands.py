b9d6be3b25251e764ae6775cc7d607fab1d28985
Don Lawson <don.lawson628@gmail.com>
2025-07-21 16:46:49 -0700

    Fix command extraction in backfill and add tests

---
+++ tests/test_backfill_commands.py
@@ -0,0 +1,14 @@
+import types
+import discord
+
+from gentlebot.backfill_commands import _extract_cmd
+
+
+def test_extract_cmd_interaction():
+    msg = types.SimpleNamespace(content="irrelevant", interaction=types.SimpleNamespace(name="foo"))
+    assert _extract_cmd(msg) == "foo"
+
+
+def test_extract_cmd_fallback():
+    msg = types.SimpleNamespace(content="/bar baz", interaction=None)
+    assert _extract_cmd(msg) == "bar"
